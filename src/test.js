fe4adb089f934c2801fbb9ffece80afa77af7c6f
Filipp Gostev <fgostev@gmail.com>
2021-08-21 19:14:58 -0500

    test build

--- src/test.js
+++ src/test.js
@@ -1,8 +1,9 @@
+import queretaro from './queretaro.jpeg'
 
 function test (){
     const content = document.getElementById('content');
     const img = document.createElement('img');
-    img.src = './queretaro.jpeg';
+    img.src = queretaro;
     content.appendChild(img);
 }
 
